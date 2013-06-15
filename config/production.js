88fe9703f50cd3636efb5bbf632a0bae3db4d775
Anthony Nowell <anowell@gmail.com>
2013-06-14 23:20:45 -0700

    refactor out mongo and host URIs

--- config/production.js
+++ config/production.js
@@ -1,10 +1,10 @@
 module.exports = {
   db: {
-    uri:        process.env.MONGOLAB_URI
+    uri:        process.env.DB_URI
   },
   facebook: {
     appId:      process.env.FACEBOOK_APP_ID,
     appSecret:  process.env.FACEBOOK_SECRET,
-    hostUri:    "http://prayercloud.herokuapp.com"
+    hostUri:    process.env.HOST_URI
   }
 }
